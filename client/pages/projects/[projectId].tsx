61c84cb0cf9d8c80caabd80e740bbb249eb87773
Lorenze Jay Hernandez <lorenzejaytech@gmail.com>
2021-12-27 14:36:39 -0800

    updated to add tasks associated for every individual project

--- client/pages/projects/[projectId].tsx
+++ client/pages/projects/[projectId].tsx
@@ -1,45 +1,110 @@
 import axios from "axios";
 import { useRouter } from "next/router";
-import React, { FormEvent, useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useMutation, useQuery, useQueryClient } from "react-query";
 import ProjectDetailsForm from "../../components/app/Projects/projectDetails";
-import { useAuth } from "../../hooks/useAuth";
-import { ProjectDetails, ProjectStatus } from "../../types/projectTypes";
-import { TaskDetails } from "../../types/tasksTypes";
-import { ClientDetails, FormType, ApiCallReturn } from "../../types/userTypes";
+import { ProjectDetails } from "../../types/projectTypes";
 import Link from "next/link";
+import useFirebaseAuth from "../../hooks/useAuth3";
+import { FormType, TaskDetails } from "../../types/tasksTypes";
 
 const Project = () => {
+  const queryClient = useQueryClient();
   const router = useRouter();
   const { projectId } = router.query;
   // const queryClient = useQueryClient();
-  const { userId, userToken } = useAuth();
+  const { authUser, loading } = useFirebaseAuth();
 
   useEffect(() => {
-    if (!userId) {
-      router.push("/sign-in");
+    if (!authUser && !loading) {
+      router.push("/home");
     }
-  }, [userId]);
+  }, [authUser, loading]);
 
   const fetchProjectDetails = async () => {
+    if (!authUser?.token) return;
+
     const config = {
       headers: {
         "Content-Type": "application/json",
-        token: userToken,
+        token: authUser.token,
       },
     };
-    if (!projectId) return;
+
     const { data } = await axios.get(
       `/api/projects/project-details/${projectId}`,
       config
     );
     return data.data;
   };
+
+  //get the tasks associated with the project
+  const fetchAssociatedTasks = async () => {
+    try {
+      if (!authUser?.token) return;
+
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          token: authUser.token,
+        },
+      };
+      const { data } = await axios.get(
+        `/api/projects/list-project-tasks/${projectId}`,
+        config
+      );
+      if (data.success) {
+        return data.data;
+      }
+    } catch (error) {
+      return error;
+    }
+  };
+
   const { data: projectDetails } = useQuery<ProjectDetails>(
-    `client-details-${projectId}`,
+    `project-details-${authUser?.uid}-${projectId}`,
     fetchProjectDetails
   );
+  const { data: tasks } = useQuery<TaskDetails[]>(
+    `project-tasks-${authUser?.uid}-${projectId}`,
+    fetchAssociatedTasks
+  );
 
+  // console.log("tasks", tasks);
+  const updateTaskStatus = async ({
+    taskId,
+    currentStatus,
+  }: {
+    taskId: Number;
+    currentStatus: FormType;
+  }) => {
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          token: authUser?.token,
+        },
+      };
+      const newStatus =
+        currentStatus === FormType.completed
+          ? FormType.incomplete
+          : FormType.completed;
+
+      await axios.post(
+        `/api/tasks/update-task-status/${taskId}`,
+        { newStatus },
+        config
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const { mutateAsync: handleUpdateTaskStatus } = useMutation(
+    updateTaskStatus,
+    {
+      onSuccess: () => queryClient.invalidateQueries(`tasks`),
+    }
+  );
   return (
     <>
       <Link href="/projects">
@@ -53,6 +118,37 @@ const Project = () => {
           projectId={projectId?.toString()}
         />
       )}
+      <section className="w-full px-5 py-3 lg:w-1/2 mx-auto ">
+        <h3 className="text-3xl mb-5 tracking-wide ">Tasks</h3>
+        {(!tasks || tasks?.length === 0) && <p>No tasks</p>}
+        <ul className="">
+          {tasks &&
+            tasks.map((task) => (
+              <div
+                key={task.id}
+                className="flex items-center shadow-2xl bg-white my-2 rounded-md p-5 "
+              >
+                <input
+                  type="checkbox"
+                  className="mr-3"
+                  onClick={(e) => {
+                    e.target;
+                    handleUpdateTaskStatus({
+                      taskId: task.id,
+                      currentStatus: task.status,
+                    });
+                  }}
+                  defaultChecked={
+                    task.status === FormType.completed ? true : false
+                  }
+                  id={task.id.toString()}
+                  name={task.description}
+                />
+                <label htmlFor={task.description}>{task.description}</label>
+              </div>
+            ))}
+        </ul>
+      </section>
     </>
   );
 };
