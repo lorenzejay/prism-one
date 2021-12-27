ad49778462c886de18cda026b18c967f5347482f
Lorenze Jay Hernandez <lorenzejaytech@gmail.com>
2021-12-27 14:35:19 -0800

    created select associated project dropdown for task components

---
+++ client/components/app/Task/SelectAssociatedProject.tsx
@@ -0,0 +1,78 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useQuery } from "react-query";
+import useFirebaseAuth from "../../../hooks/useAuth3";
+interface SelectAssociatedProjectProps {
+  createMode: boolean;
+  defaultProject?: number;
+  defaultProjects?: { title: string; id: number }[];
+  projectAssociated: number | undefined;
+  setProjectAssociated: (x: number) => void;
+}
+const SelectAssociatedProject = ({
+  createMode,
+  defaultProject,
+  defaultProjects,
+  projectAssociated,
+  setProjectAssociated,
+}: SelectAssociatedProjectProps) => {
+  const { authUser } = useFirebaseAuth();
+  //list out projects to select from associated -> map that on select
+
+  const getProjectToAssociateTaskTo = async () => {
+    if (!authUser?.token) return;
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        token: authUser.token,
+      },
+    };
+    const { data } = await axios.get(
+      "/api/tasks/list-associated-projects",
+      config
+    );
+    return data.data;
+  };
+  const { data: projects } = useQuery<{ title: string; id: number }[]>(
+    `project-association-${authUser?.uid}`,
+    getProjectToAssociateTaskTo
+  );
+  useEffect(() => {
+    if (defaultProject) {
+      setProjectAssociated(defaultProject);
+    }
+  }, [defaultProject]);
+
+  return (
+    <>
+      <label htmlFor="ProjectAssociated" className="text-lg">
+        Project Associated:
+      </label>
+      <select
+        name="ProjectAssociated"
+        className="rounded-md p-2 mb-4"
+        // defaultValue={defaultProject || ""}
+        onChange={(e) => setProjectAssociated(parseInt(e.target.value))}
+        value={projectAssociated}
+      >
+        <option value={""}></option>
+        {projects &&
+          createMode &&
+          projects.map((sub, i) => (
+            <option value={sub.id} key={i}>
+              {sub.title}
+            </option>
+          ))}
+        {!createMode &&
+          defaultProjects &&
+          defaultProjects.map((sub, i) => (
+            <option value={sub.id} key={i}>
+              {sub.title}
+            </option>
+          ))}
+      </select>
+    </>
+  );
+};
+
+export default SelectAssociatedProject;
