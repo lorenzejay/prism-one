4686b1592a8db8764ca9a73995d860deb463758e
Lorenze Jay Hernandez <lorenzejaytech@gmail.com>
2021-12-28 07:31:12 -0800

    cleanup

--- client/pages/projects/[projectId].tsx
+++ client/pages/projects/[projectId].tsx
@@ -1,18 +1,17 @@
 import axios from "axios";
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
-import { useMutation, useQuery, useQueryClient } from "react-query";
+import { useQuery } from "react-query";
 import ProjectDetailsForm from "../../components/app/Projects/projectDetails";
 import { ProjectDetails } from "../../types/projectTypes";
 import Link from "next/link";
 import useFirebaseAuth from "../../hooks/useAuth3";
-import { FormType, TaskDetails } from "../../types/tasksTypes";
+import { TaskDetails } from "../../types/tasksTypes";
+import ProjectTask from "../../components/app/Projects/ProjectTask";
 
 const Project = () => {
-  const queryClient = useQueryClient();
   const router = useRouter();
   const { projectId } = router.query;
-  // const queryClient = useQueryClient();
   const { authUser, loading } = useFirebaseAuth();
 
   useEffect(() => {
@@ -61,50 +60,15 @@ const Project = () => {
     }
   };
 
-  const { data: projectDetails } = useQuery<ProjectDetails>(
-    `project-details-${authUser?.uid}-${projectId}`,
-    fetchProjectDetails
-  );
-  const { data: tasks } = useQuery<TaskDetails[]>(
-    `project-tasks-${authUser?.uid}-${projectId}`,
-    fetchAssociatedTasks
-  );
+  const { data: projectDetails, isLoading: pDetailsLoading } =
+    useQuery<ProjectDetails>(
+      `project-details-${authUser?.uid}-${projectId}`,
+      fetchProjectDetails
+    );
+  const { data: tasks, isLoading: tasksLoading } = useQuery<
+    TaskDetails[] | null
+  >(`project-tasks-${authUser?.uid}-${projectId}`, fetchAssociatedTasks);
 
-  // console.log("tasks", tasks);
-  const updateTaskStatus = async ({
-    taskId,
-    currentStatus,
-  }: {
-    taskId: Number;
-    currentStatus: FormType;
-  }) => {
-    try {
-      const config = {
-        headers: {
-          "Content-Type": "application/json",
-          token: authUser?.token,
-        },
-      };
-      const newStatus =
-        currentStatus === FormType.completed
-          ? FormType.incomplete
-          : FormType.completed;
-
-      await axios.post(
-        `/api/tasks/update-task-status/${taskId}`,
-        { newStatus },
-        config
-      );
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  const { mutateAsync: handleUpdateTaskStatus } = useMutation(
-    updateTaskStatus,
-    {
-      onSuccess: () => queryClient.invalidateQueries(`tasks`),
-    }
-  );
   return (
     <>
       <Link href="/projects">
@@ -112,43 +76,22 @@ const Project = () => {
           Return
         </h3>
       </Link>
+
       {projectId && (
         <ProjectDetailsForm
           projectDetails={projectDetails}
           projectId={projectId?.toString()}
         />
       )}
-      <section className="w-full px-5 py-3 lg:w-1/2 mx-auto ">
-        <h3 className="text-3xl mb-5 tracking-wide ">Tasks</h3>
-        {(!tasks || tasks?.length === 0) && <p>No tasks</p>}
-        <ul className="">
-          {tasks &&
-            tasks.map((task) => (
-              <div
-                key={task.id}
-                className="flex items-center shadow-2xl bg-white my-2 rounded-md p-5 "
-              >
-                <input
-                  type="checkbox"
-                  className="mr-3"
-                  onClick={(e) => {
-                    e.target;
-                    handleUpdateTaskStatus({
-                      taskId: task.id,
-                      currentStatus: task.status,
-                    });
-                  }}
-                  defaultChecked={
-                    task.status === FormType.completed ? true : false
-                  }
-                  id={task.id.toString()}
-                  name={task.description}
-                />
-                <label htmlFor={task.description}>{task.description}</label>
-              </div>
-            ))}
-        </ul>
-      </section>
+      {tasks && <ProjectTask tasks={tasks} taskLoading={tasksLoading} />}
+
+      {tasks === null && (
+        <section className="w-1/2 mx-auto">
+          <h3 className="text-3xl mb-5 tracking-wide ">Tasks</h3>
+
+          <p className="">No tasks</p>
+        </section>
+      )}
     </>
   );
 };
