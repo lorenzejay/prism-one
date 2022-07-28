13c36e97f763a9148fb6b6868968ac4aaf11ebef
Lorenze Jay <lorenzejaytech@gmail.com>
2022-07-27 17:15:46 -0700

    using experimental

--- client/next.config.js
+++ client/next.config.js
@@ -1,22 +1,24 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  async rewrites() {
-    const baseUrl = process.env.NEXT_PUBLIC_BACKEND_URL_PROD;
-    if (process.env.NEXT_PUBLIC_NODE_ENV === "production") {
-      return [
-        {
-          source: "/api/:slug*",
-          destination: `${baseUrl}/api/:slug*`,
-        },
-      ];
-    } else {
-      const baseUrl = process.env.NEXT_PUBLIC_BACKEND_URL_DEV;
-      return [
-        {
-          source: "/api/:slug*",
-          destination: `${baseUrl}/api/:slug*`,
-        },
-      ];
-    }
+  experimental: {
+    async rewrites() {
+      // const baseUrl = process.env.NEXT_PUBLIC_BACKEND_URL_PROD;
+      if (process.env.NEXT_PUBLIC_NODE_ENV === "production") {
+        return [
+          {
+            source: "/api/:slug*",
+            destination: `${process.env.NEXT_PUBLIC_BACKEND_URL_PROD}/api/:slug*`,
+          },
+        ];
+      } else {
+        // const baseUrl = process.env.NEXT_PUBLIC_BACKEND_URL_DEV;
+        return [
+          {
+            source: "/api/:slug*",
+            destination: `${process.env.NEXT_PUBLIC_BACKEND_URL_DEV}/api/:slug*`,
+          },
+        ];
+      }
+    },
   },
 };
