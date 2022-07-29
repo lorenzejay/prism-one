c554a99b8766f8ecf9489cbcb1f6aa7e32d4ac0f
Lorenze Jay <lorenzejaytech@gmail.com>
2022-07-28 21:45:45 -0700

    revert to manual write on destination

--- client/next.config.js
+++ client/next.config.js
@@ -1,22 +1,21 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  experimental: {
-    async rewrites() {
-      if (process.env.NEXT_PUBLIC_NODE_ENV === "production") {
-        return [
-          {
-            source: "/api/:slug*",
-            destination: `${process.env.NEXT_PUBLIC_BACKEND_URL_PROD}/api/:slug*`,
-          },
-        ];
-      } else {
-        return [
-          {
-            source: "/api/:slug*",
-            destination: `${process.env.NEXT_PUBLIC_BACKEND_URL_DEV}/api/:slug*`,
-          },
-        ];
-      }
-    },
+  async rewrites() {
+    if (process.env.NEXT_PUBLIC_NODE_ENV === "production") {
+      return [
+        {
+          source: "/api/:slug*",
+          destination: `https://prism-one-crm-backend.herokuapp.com/api/:slug*`,
+        },
+      ];
+    } else {
+      const baseUrl = "http://localhost:5001";
+      return [
+        {
+          source: "/api/:slug*",
+          destination: `${baseUrl}/api/:slug*`,
+        },
+      ];
+    }
   },
 };
